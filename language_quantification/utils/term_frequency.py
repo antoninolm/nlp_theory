4b649fe1fc08ce5fdd3c664eac915ffdb94d9e16
Antonino La Manna <anto.lamanna1@gmail.com>
2025-11-30 19:34:05 +0100

    Update the repo with latest contents

---
+++ language_quantification/utils/term_frequency.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from utils.preprocessing1 import preprocess_text
+from utils.poems import poems
+
+# preprocess text
+processed_poems = [preprocess_text(poem) for poem in poems]
+
+# initialize and fit CountVectorizer
+vectorizer1 = CountVectorizer()
+term_frequencies = vectorizer1.fit_transform(processed_poems)
+
+# get vocabulary of terms
+feature_names = vectorizer1.get_feature_names_out()
+
+# get corpus index
+corpus_index = [f"Poem {i+1}" for i in range(len(poems))]
+
+# create pandas DataFrame with term frequencies
+df_term_frequencies = pd.DataFrame(term_frequencies.T.todense(), index=feature_names, columns=corpus_index)
+
+# initialize and fit CountVectorizer
+vectorizer2 = CountVectorizer()
+bow_matrix = vectorizer2.fit_transform(processed_poems)
+
+# get vocabulary of terms
+feature_names = vectorizer2.get_feature_names_out()
+
+# get corpus index
+corpus_index = [f"Poem {i+1}" for i in range(len(poems))]
+
+# create pandas DataFrame with term frequencies
+df_bag_of_words = pd.DataFrame(bow_matrix.T.todense(), index=feature_names, columns=corpus_index)
